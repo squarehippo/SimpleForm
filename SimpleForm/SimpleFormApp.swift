3c601d2fb20c45bae454895c3f631771dd8a076b
Brian Wilson <squarehippobills@gmail.com>
2021-06-09 12:43:03 -0400

    Initial Commit

---
+++ SimpleForm/SimpleFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFormApp.swift
+//  SimpleForm
+//
+//  Created by Brian Wilson on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
